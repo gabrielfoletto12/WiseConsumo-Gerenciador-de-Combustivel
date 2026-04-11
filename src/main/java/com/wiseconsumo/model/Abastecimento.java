ca4539832c5539af377e3d1d8ac4f6fd27238c91
gabrielfoletto12 <gabrielfoletto@sempreceub.com>
2026-04-11 10:01:48 -0300

    Criado modelo de dados Abastecimento e definido versão 0.1.0 de desesnvolvimento

---
+++ src/main/java/com/wiseconsumo/model/Abastecimento.java
@@ -0,0 +1,53 @@
+package com.wiseconsumo.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Abastecimento {
+    private double quilometragemAtual;
+    private double litrosAbastecidos;
+    private BigDecimal valorGasto;
+    private LocalDate data;
+
+
+    public Abastecimento(double quilometragemAtual, double litrosAbastecidos, BigDecimal valorGasto, LocalDate data ){
+        this.quilometragemAtual = quilometragemAtual;
+        this.litrosAbastecidos = litrosAbastecidos;
+        this.valorGasto = valorGasto;
+        this.data = data;
+    }
+
+    public double getQuilometragemAtual(){
+        return quilometragemAtual;
+    }
+
+    public double getLitrosAbastecidos(){
+        return litrosAbastecidos;
+    }
+
+    public BigDecimal getValorGasto(){
+        return valorGasto;
+    }
+
+    public LocalDate getData(){
+        return data;
+    }
+
+    public void setQuilometragemAtual(double quilometragemAtual){
+        this.quilometragemAtual = quilometragemAtual;
+    }
+
+    public void setLitrosAbastecidos(double litrosAbastecidos){
+        this.litrosAbastecidos = litrosAbastecidos;
+    }
+    public void setValorGasto(BigDecimal valorGasto){
+        this.valorGasto = valorGasto;
+    }
+    public void setData(LocalDate data){
+        this.data = data;
+    }
+
+
+}
+
+
